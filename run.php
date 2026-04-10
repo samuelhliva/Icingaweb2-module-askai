b05d40034de250575a725034c9cfa245c2627503
Samuel Hliva <samuel.hliva@syntax.com>
2026-04-10 11:42:53 +0200

    icingadb hook registration

--- run.php
+++ run.php
@@ -2,6 +2,14 @@
 /* Icinga Web 2 - AskAI Module | (c) 2025 Samuel Hliva | GPLv2+ */
 
 /** @var $this \Icinga\Application\Modules\Module */
+use Icinga\Module\Grafana\ProvidedHook\Icingadb\IcingadbSupport;
 
-$this->provideHook('Monitoring/HostActions');
-$this->provideHook('Monitoring/ServiceActions');
+if ($this->exists('monitoring')){
+    $this->provideHook('Monitoring/HostActions');
+    $this->provideHook('Monitoring/ServiceActions');
+}
+
+if ($this->exists('icingadb')){
+    $this->provideHook('icingadb/HostActions');
+    $this->provideHook('icingadb/ServiceActions');
+}
